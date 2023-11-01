47d17eb94da3f136ecc80ac70e17f042e163d8a7
unknown <m.alinkon10@gmail.com>
2023-11-01 16:22:08 +0600

    nuxt working

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <main>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <ContentDoc />
+    <template>
+      <div>
+        <h1>Welcome to the homepage</h1>
+        <AppAlert>
+          This is an auto-imported component
+        </AppAlert>
+      </div>
+    </template>
+    <!-- <ContentDoc /> -->
   </main>
 </template>
