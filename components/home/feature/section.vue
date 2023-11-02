8d523423db245a12eacf611743e2adf588cb123d
unknown <m.alinkon10@gmail.com>
2023-11-02 11:26:00 +0600

    features section added

---
+++ components/home/feature/section.vue
@@ -0,0 +1,94 @@
+<template>
+    <section class="h-[1160px] lg:h-[920px] bg-[#FFF]">
+        <section class="pt-10">
+            <div class="flex justify-center">
+                <p class="font-bold text-[24px]">Our Features</p>
+            </div>
+            <div class="p-2 text-center flex justify-center">
+                <p class="font-400 pt-2 w-[924px]">Fully automated Garage System(GMS) is a cloud-based mobile and responsive
+                    web
+                    application for garage
+                    management with CMS.</p>
+            </div>
+        </section>
+        <section class="grid grid-cols-1 lg:grid-cols-2 ">
+            <div class="h-full flex justify-center lg:justify-end ">
+                <div class="mt-5 w-10/12 ">
+                    <div class="flex justify-start lg:justify-center pt-8">
+                        <img class="lg:hidden" src="~/assets/img/feature-section.png" alt="">
+                        <img class="hidden lg:block w-[639px] h-[509px]" src="~/assets/img/feature-section-pc.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="h-full flex justify-start lg:justify-center ">
+                <div class="ps-4 pt-8 lg:pt-16 w-12/12 lg:w-10/12">
+                    <div class="pt-5">
+                        <p class="font-bold text-[24px]">Service Center</p>
+                    </div>
+                    <div class="">
+                        <p class="font-400 pt-2">Schedule Appointment, Estimate, Inspection & Health card, Job Card,
+                            Invoice,
+                            Order
+                            Summary, Customer & Vehicle Master, and More Schedule Appointment, Estimate, Inspection & Health
+                            card,
+                            Job Card, Invoice, Order Summary.</p>
+                    </div>
+                    <div class="pt-8 lg:pt-8">
+                        <div class=" w-full">
+                            <div class="flex items-center">
+                                <img src="~/assets/img/ficon1.png" alt="">
+                                <p class="font-bold text-2xl ps-4">Vehicle Entries</p>
+                            </div>
+                            <p class="ps-16 pe-2">Schedule Appointment, Estimate, Inspection & Health card, Job Card, Invoice, Order Summary.</p>
+                        </div>
+                        <div class="">
+                            <div class=" flex items-center">
+                                <img src="~/assets/img/ficone2.png" alt="">
+                                <p class="font-bold text-2xl ps-4">Repairing Vehicle</p>
+                            </div>
+                            <p class="ps-16 pe-2">Schedule Appointment, Estimate, Inspection & Health card, Job Card, Invoice, Order Summary, Customer & Vehicle Master.</p>
+                        </div>
+                        <div class="">
+                            <div class="flex items-center">
+                                <img src="~/assets/img/ficon3.png" alt="">
+                                <p class="font-bold text-2xl ps-4">Departure</p>
+                            </div>
+                            <p class="ps-16 pe-2">Schedule Appointment, Estimate, Inspection & Health card, Job Card.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+</template>
+
+            <!-- left -->
+            <!-- <div>
+                <div class="mt-5">
+                    <div class="flex justify-center">
+                        <p class="font-bold text-[24px]">Our Features</p>
+                    </div>
+                    <div class="p-2 text-center">
+                        <p class="font-400 pt-2">Fully automated Garage System(GMS) is a cloud-based mobile and responsive
+                            web
+                            application for garage
+                            management with CMS.</p>
+                    </div>
+                    <div class="flex justify-center pt-8">
+                        <img src="~/assets/img/feature-section.png" alt="">
+                    </div>
+                </div>
+            </div> -->
+            <!-- right section -->
+            <!-- <div>
+                <div class="flex justify-center pt-5">
+                    <p class="font-bold text-[24px]">Service Center</p>
+                </div>
+                <div class="p-2 text-center">
+                    <p class="font-400 pt-2">Schedule Appointment, Estimate, Inspection & Health card, Job Card, Invoice,
+                        Order
+                        Summary, Customer & Vehicle Master, and More Schedule Appointment, Estimate, Inspection & Health
+                        card,
+                        Job Card, Invoice, Order Summary.</p>
+                </div>
+            </div> -->
