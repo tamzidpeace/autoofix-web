c567ee85f3dc1606ab0e1503df1c7580c75b25db
unknown <m.alinkon10@gmail.com>
2023-11-02 09:47:27 +0600

    animation remove

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <HomeHeroSection />
-    <HomePriceSection />
     <HomeDownloadSection />
+    <HomePriceSection />
     <h1 class="text-3xl font-bold underline">Hello world !</h1>
     <!-- <template>
       <div>
