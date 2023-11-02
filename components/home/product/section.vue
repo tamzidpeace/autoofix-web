726fd91261af4a22e8374d7b56ab56bc312ea2cb
unknown <m.alinkon10@gmail.com>
2023-11-02 12:22:24 +0600

    product section web created

---
+++ components/home/product/section.vue
@@ -0,0 +1,100 @@
+<template>
+    <!-- for web -->
+    <section class="hidden lg:block h-[723px] bg-[#F5F7FA]">
+        <div class="grid grid-cols-2 h-full">
+            <div class="flex items-center justify-end ">
+                <div class="w-[685px]">
+                    <div class="">
+                        <p class="text-[36px] font-[700]">Products</p>
+                        <p>As suggested by the name, wholesale auto spare parts are units that automobile owners can use as
+                            replacements to exchange for a dysfunctional piece of their</p>
+                    </div>
+                    <div class="pt-10">
+                        <div class="grid grid-cols-2">
+                            <div class="">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/p-chesis.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Chassis</p>
+                                </div>
+                                <p class="ps-16">Foundation structure of any car that supports it from underneath.</p>
+                            </div>
+                            <div class="">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/p-engine.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Engine</p>
+                                </div>
+                                <p class="ps-16">Produces combustion and moves the pistons</p>
+                            </div>
+                            <div class="">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/p-trans.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Transmission System</p>
+                                </div>
+                                <p class="ps-16">Under the front hood as part of the drive-train.</p>
+                            </div>
+                            <div class="">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/p-body.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">The Body</p>
+                                </div>
+                                <p class="ps-16">Roof, hood, fenders, trunk lid, doors, and windows.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-center">
+                <img src="~/assets/img/product-section.png" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- for phone -->
+    <section class=" h-[980px] lg:hidden bg-[#F5F7FA]">
+        <div class="pt-12">
+            <div class="flex justify-center">
+                <div class="w-[343px]">
+                    <p class="text-[24px] font-[700] text-center">Products</p>
+                    <p class="text-center">As suggested by the name, wholesale auto spare parts are units that automobile
+                        owners can use as
+                        replacements to exchange for a dysfunctional piece of their</p>
+
+                </div>
+            </div>
+            <div class="flex justify-center pt-8">
+                <img src="~/assets/img/product-mobile-iamge.png" alt="">
+            </div>
+            <div>
+                <div class="grid grid-cols-1 p-6 pt-10">
+                    <div class="">
+                        <div class="flex items-center">
+                            <img class="w-[40px]" src="~/assets/img/p-chesis.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Chassis</p>
+                        </div>
+                        <p class="ps-16">Foundation structure of any car that supports it from underneath.</p>
+                    </div>
+                    <div class="">
+                        <div class="flex items-center ">
+                            <img class="w-[40px]" src="~/assets/img/p-engine.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Engine</p>
+                        </div>
+                        <p class="ps-16">Produces combustion and moves the pistons</p>
+                    </div>
+                    <div class="">
+                        <div class="flex items-center">
+                            <img class="w-[40px]" src="~/assets/img/p-trans.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Transmission System</p>
+                        </div>
+                        <p class="ps-16">Under the front hood as part of the drive-train.</p>
+                    </div>
+                    <div class="">
+                        <div class="flex items-center">
+                            <img class="w-[40px]" src="~/assets/img/p-body.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">The Body</p>
+                        </div>
+                        <p class="ps-16">Roof, hood, fenders, trunk lid, doors, and windows.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <main>
     <HomeHeroSection />
     <HomeFeatureSection />
+    <HomeProductSection />
     <HomeDownloadSection />
     <HomePriceSection />
     <h1 class="text-3xl font-bold underline">Hello world !</h1>
