13cd4e8deb59428e52a206a3dffadeec8642bd37
unknown <m.alinkon10@gmail.com>
2023-11-03 18:23:29 +0600

    added the invoice and quotation report section

---
+++ components/home/quotation/section.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- for web -->
+    <section class="hidden lg:block h-[682px] bg-[#F5F6F6]">
+        <div class="grid grid-cols-2 h-full">
+            <div class="flex items-center justify-end ">
+                <div class="w-[685px]">
+                    <div class="">
+                        <p class="text-[36px] font-[700]">Build Professional Price Quotation.</p>
+                        <p>Free quotation maker - create quotation & estimate online with quotation templates. Send email &
+                            download pdf, no watermark with online quotation generator. Making a good quotation requires
+                            in-depth research for estimates, costs</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center justify-center">
+                <img src="~/assets/img/quatation.png" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- for phone -->
+    <section class=" h-[700px] lg:hidden bg-[#F6F8FF]">
+        <div class="">
+            <div class="flex justify-center ">
+                <img src="~/assets/img/quatation.png" alt="">
+            </div>
+            <div class="flex justify-start">
+                <div class="w-[343px] ps-8">
+                    <p class="text-[24px] font-[700] text-start">Build Professional Price Quotation.</p>
+                    <p class="text-start">Free quotation maker - create quotation & estimate online with quotation
+                        templates. Send email &
+                        download pdf, no watermark with online quotation generator. Making a good quotation requires
+                        in-depth research for estimates, costs</p>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+</template>
---
+++ components/home/report/section.vue
@@ -0,0 +1,81 @@
+<template>
+    <!-- for web -->
+    <section class="hidden lg:block h-[727px] bg-[#FFF]">
+        <div class="grid grid-cols-2 h-full">
+            <div class="flex items-center justify-center">
+                <img src="~/assets/img/report.png" alt="">
+            </div>
+            <div class="flex items-center justify-start ">
+                <div class="w-[685px]">
+                    <div class="">
+                        <p class="text-[36px] font-[700]">Reports</p>
+                        <p>Create unlimited invoices with professional and personalized templates. Automate reminders and
+                            recurring invoices and get paid on time swimmingly.</p>
+                    </div>
+                    <div class="pt-10">
+                        <div class="grid grid-cols-1">
+                            <div class="pb-4">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/r1.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Inventory Reports</p>
+                                </div>
+                            </div>
+                            <div class="pb-4">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/r2.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Sales Reports</p>
+                                </div>
+                            </div>
+                            <div class="pb-4">
+                                <div class="flex items-center">
+                                    <img src="~/assets/img/r3.png" alt="">
+                                    <p class="text-[24px] font-[700] ps-4">Customer Reports</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- for phone -->
+    <section class=" h-[900px] lg:hidden bg-[#FFF]">
+        <div class="pt-12">
+            <div class="flex justify-center">
+                <div class="w-[343px]">
+                    <p class="text-[24px] font-[700] text-center">Reports</p>
+                    <p class="text-center">Create unlimited invoices with professional and personalized templates. Automate
+                        reminders and
+                        recurring invoices and get paid on time swimmingly</p>
+
+                </div>
+            </div>
+            <div class="flex justify-center p-8">
+                <img src="~/assets/img/report.png" alt="">
+            </div>
+            <div>
+                <div class="grid grid-cols-1 p-6 pt-10">
+                    <div class="pb-4">
+                        <div class="flex items-center">
+                            <img class="w-[40px]" src="~/assets/img/r1.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Inventory Reports</p>
+                        </div>
+                    </div>
+                    <div class="pb-4">
+                        <div class="flex items-center ">
+                            <img class="w-[40px]" src="~/assets/img/r2.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Work Assigned</p>
+                        </div>
+                    </div>
+                    <div class="pb-4">
+                        <div class="flex items-center ">
+                            <img class="w-[40px]" src="~/assets/img/r3.png" alt="">
+                            <p class="text-[20px] font-[700] ps-4">Customer Reports</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template> 
